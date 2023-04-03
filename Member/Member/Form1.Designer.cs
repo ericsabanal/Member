2847650a549284464e68e1da9c885036566aea64
Eric De Leon <ericsabanaldeleon5291989@gmail.com>
2023-04-03 19:03:39 +0800

    Fixing textbox error in numbers and letters

--- Member/Member/Form1.Designer.cs
+++ Member/Member/Form1.Designer.cs
@@ -38,12 +38,14 @@
             this.btnInsert = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.btnRead = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnExit = new System.Windows.Forms.Button();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -92,6 +94,7 @@
             this.txtid.Name = "txtid";
             this.txtid.Size = new System.Drawing.Size(260, 20);
             this.txtid.TabIndex = 4;
+            this.txtid.TextChanged += new System.EventHandler(this.txtid_TextChanged);
             // 
             // txtName
             // 
@@ -99,6 +102,8 @@
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(260, 20);
             this.txtName.TabIndex = 5;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            this.txtName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtName_KeyPress);
             // 
             // txtAge
             // 
@@ -106,10 +111,11 @@
             this.txtAge.Name = "txtAge";
             this.txtAge.Size = new System.Drawing.Size(260, 20);
             this.txtAge.TabIndex = 6;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged_1);
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(15, 177);
+            this.btnInsert.Location = new System.Drawing.Point(79, 177);
             this.btnInsert.Name = "btnInsert";
             this.btnInsert.Size = new System.Drawing.Size(75, 23);
             this.btnInsert.TabIndex = 7;
@@ -119,7 +125,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(96, 177);
+            this.button2.Location = new System.Drawing.Point(171, 177);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
@@ -129,23 +135,13 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(185, 177);
+            this.button3.Location = new System.Drawing.Point(264, 177);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 9;
             this.button3.Text = "DELETE";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // btnRead
-            // 
-            this.btnRead.Location = new System.Drawing.Point(266, 177);
-            this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(75, 23);
-            this.btnRead.TabIndex = 10;
-            this.btnRead.Text = "Read";
-            this.btnRead.UseVisualStyleBackColor = true;
-            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
             // 
             // dataGridView1
             // 
@@ -156,43 +152,53 @@
             this.dataGridView1.TabIndex = 11;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(96, 206);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 12;
-            this.btnExit.Text = "EXIT";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
             // txtSearch
             // 
             this.txtSearch.Location = new System.Drawing.Point(369, 53);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(313, 20);
+            this.txtSearch.Size = new System.Drawing.Size(394, 20);
             this.txtSearch.TabIndex = 13;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
-            // btnSearch
+            // menuStrip1
             // 
-            this.btnSearch.Location = new System.Drawing.Point(688, 53);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnSearch.TabIndex = 14;
-            this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearch);
-            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnRead);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnInsert);
@@ -203,10 +209,14 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -224,11 +234,12 @@
         private System.Windows.Forms.Button btnInsert;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button btnRead;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.TextBox txtSearch;
-        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
 
--- Member/Member/Form1.cs
+++ Member/Member/Form1.cs
@@ -21,26 +21,34 @@ namespace Member
             InitializeComponent();
         }
 
+      
 
         //https://www.youtube.com/watch?v=Vfr6dS8DjOY&ab_channel=PinoyFreeCoder
         private void button1_Click(object sender, EventArgs e)
         {
+
+            if (string.IsNullOrEmpty(txtid.Text) || string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Value is Required", "Message Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
+                con.Open();
+
+                SQLiteCommand cmd = new SQLiteCommand("insert into User_Table(ID,Name,Age) VALUES (@ID,@Name,@Age)", con);
+                cmd.Parameters.AddWithValue("@ID", int.Parse(txtid.Text));
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Age", txtAge.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                txtid.Text = "";
+                txtName.Text = "";
+                txtAge.Text = "";
+                MessageBox.Show(" Successfully Inserted");
+            }
            
-            SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
-            con.Open(); 
-
-            SQLiteCommand cmd = new SQLiteCommand("insert into User_Table(ID,Name,Age) VALUES (@ID,@Name,@Age)",con);
-            cmd.Parameters.AddWithValue("@ID",int.Parse(txtid.Text));
-            cmd.Parameters.AddWithValue("@Name",txtName.Text);
-            cmd.Parameters.AddWithValue("@Age",txtAge.Text);
-            cmd.ExecuteNonQuery();
-            con.Close();
-
-            txtid.Text = "";
-            txtName.Text = "";
-            txtAge.Text = "";
-            MessageBox.Show(" Successfully Inserted");
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -81,6 +89,9 @@ namespace Member
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //    dataGridView1.Rows.Clear();
+            //    dataGridView1.Refresh();
+
             SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
             con.Open();
 
@@ -89,6 +100,9 @@ namespace Member
             DataTable dt = new DataTable();
             da.Fill(dt);
             dataGridView1.DataSource = dt;
+
+
+            
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -108,7 +122,6 @@ namespace Member
 
             SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
             con.Open();
-
             SQLiteCommand cmd = new SQLiteCommand("select * from User_Table", con);
             SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -129,13 +142,98 @@ namespace Member
 
         public DataTable GetName(string name) 
         {
-            string query = string.Format("select * from User_Table where Name ='{0}'", name);
+            string query = string.Format("SELECT * FROM User_Table where Name like '%{0}%'",name);
             return DataAccess.ExecuteQuery(query);
         }
-        private void btnSearch_Click(object sender, EventArgs e)
+       
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = GetName(txtName.Text.Trim());
-          
+            Form1 form = new Form1();
+           dataGridView1.DataSource= form.GetName(txtSearch.Text.Trim());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            iExit = MessageBox.Show("Confirm if you want to exit", "DatagridView", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (iExit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            iExit = MessageBox.Show("Confirm if you want to Exit","DataGridView",MessageBoxButtons.YesNo,MessageBoxIcon.Information);
+
+            if (iExit == DialogResult.Yes) 
+            {
+                Application.Exit();
+            }
+        }
+
+        private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
+            con.Open();
+            SQLiteCommand cmd = new SQLiteCommand("select * from User_Table", con);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void refreshToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"Data Source = C:\Users\csf\Desktop\Member\Member\Member\bin\Debug\Member.db");
+            con.Open();
+            SQLiteCommand cmd = new SQLiteCommand("select * from User_Table", con);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+            //this accepts only numbers and hyphen
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtid.Text,"[^0-9-]")) 
+            {
+                MessageBox.Show("Please Enter only a Numbers");
+                txtid.Text = txtid.Text.Remove(txtid.Text.Length -1);
+            }
+            
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void txtAge_TextChanged_1(object sender, EventArgs e)
+        {
+            //this accepts only numbers and hyphen
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtAge.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please Enter only a Numbers");
+                txtAge.Text = txtid.Text.Remove(txtAge.Text.Length - 1);
+            }
+        }
+
+        //https://www.youtube.com/watch?v=ocD7wuF8PTg&ab_channel=InterviewPoint
+        //https://stackoverflow.com/questions/16050749/how-restrict-textbox-in-c-sharp-to-only-receive-numbers-and-dot-or-comma
+        private void txtName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !Char.IsControl(e.KeyChar) && !Char.IsWhiteSpace(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;   
+            }
         }
     }
 }
